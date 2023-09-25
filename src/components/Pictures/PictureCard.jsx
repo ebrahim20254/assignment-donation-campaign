69022fcb2eb6a8f85d3833156c370d7b98383ba4
ebrahim20254 <ebrahimkhalil125800@gmail.com>
2023-09-25 19:48:57 +0600

    header section done

---
+++ src/components/Pictures/PictureCard.jsx
@@ -0,0 +1,26 @@
+
+const PictureCard = ({picture}) => {
+    const {id, card_title, image, card_description, card_color_bg} = picture || {}
+    return (
+        <div>
+         <div className="relative flex max-w-[24rem] flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+       <div className="relative m-0 overflow-hidden rounded-none bg-transparent bg-clip-border text-gray-700 shadow-none">
+    <img
+      src={image}
+      alt="ui/ux review check"
+    />
+      </div>
+      <div className="p-6">
+      <h4 className="block font-sans text-2xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+      {card_title}
+      </h4>
+     <p className="mt-3 block font-sans text-xl font-normal leading-relaxed text-gray-700 antialiased">
+      {card_description}
+      </p>
+     </div>
+    </div>
+</div>
+    );
+};
+
+export default PictureCard;
--- src/components/Pictures/Pictures.jsx
+++ src/components/Pictures/Pictures.jsx
@@ -1,9 +1,15 @@
+import PictureCard from "./PictureCard";
 
 const Pictures = ({pictures}) => {
     console.log(pictures);
     return (
-        <div className="text-center py-6 text-4xl font-bold">
-            <h2>All Donation Pictures</h2>
+        <div className="">
+            <h2 className="py-6 text-4xl font-bold text-center">All Donation Pictures</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+                {
+                    pictures?.map((picture => <PictureCard key={picture.id} picture={picture}></PictureCard>))
+                }
+            </div>
         </div>
     );
 };
